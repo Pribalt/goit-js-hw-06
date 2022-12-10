005ea3ff1020fa2fca42061e34ae535d1ba2fe42
Pribalt <vitalisvitalik@gmail.com>
2022-12-10 21:05:22 +0200

    add_task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const textInputEl = document.querySelector('#name-input');
+const titleOutputEl = document.querySelector('#name-output');
+
+const onTargetInput = evt => {
+   titleOutputEl.textContent = evt.currentTarget.value;
+   if (evt.currentTarget.value === '') {
+      titleOutputEl.textContent = 'Anonymous';
+   }
+};
+
+textInputEl.addEventListener('input', onTargetInput);
