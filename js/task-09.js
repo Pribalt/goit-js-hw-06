5f7ce03e332aa89a2d822ae9d9af0fd83390d18c
Pribalt <vitalisvitalik@gmail.com>
2022-12-12 01:42:01 +0200

    add_task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
+const buttonChangeColorEl = document.querySelector('.change-color');
+const backgroundColorEl = document.querySelector('.color');
+const body = document.body;
+
+buttonChangeColorEl.addEventListener('click', getRandomHexColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+   return (body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`);
 }
