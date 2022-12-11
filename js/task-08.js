6f610b7a9079bfe5cacee2d13a76cc911e12e58f
Pribalt <vitalisvitalik@gmail.com>
2022-12-12 01:09:50 +0200

    add_task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onForsubmit);
+
+function onForsubmit(evt) {
+   evt.preventDefault();
+   const formElements = evt.currentTarget.elements;
+
+   const email = formElements.email.value;
+   const password = formElements.password.value;
+
+   if (email === '' || password === '') {
+      alert('WARNING! All fields must be filled');
+   } else if (email !== '' || password !== '') {
+      formEl.reset();
+   }
+
+   const formData = {
+      email,
+      password,
+   };
+
+   console.log(formData);
+}
