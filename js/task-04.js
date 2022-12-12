29699efa8c252779f3682f1de1922d5fdd1289da
Pribalt <vitalisvitalik@gmail.com>
2022-12-12 19:12:56 +0200

    fix_task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,16 +1,17 @@
-const countersEl = document.querySelectorAll('#counter button');
-const addListenerBtnEl = document.querySelector('[data-action="increment"]');
-const removeListenerBtnEl = document.querySelector('[data-action="decrement"]');
+const incrementBtnEl = document.querySelector('[data-action="increment"]');
+const decrementBtnEl = document.querySelector('[data-action="decrement"]');
 const valueCounterEl = document.querySelector('#value');
 let counterValue = 0;
 
-countersEl.forEach(el => {
-   el.addEventListener('click', e => {
-      if (el === removeListenerBtnEl) {
-         counterValue -= 1;
-      } else if (el === addListenerBtnEl) {
-         counterValue += 1;
-      }
-      valueCounterEl.textContent = counterValue;
-   });
-});
+incrementBtnEl.addEventListener('click', onAddValue);
+decrementBtnEl.addEventListener('click', onMinusValue);
+
+function onAddValue(e) {
+   counterValue += 1;
+   valueCounterEl.textContent = counterValue;
+}
+
+function onMinusValue(e) {
+   counterValue -= 1;
+   valueCounterEl.textContent = counterValue;
+}
