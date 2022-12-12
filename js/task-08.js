e9eec65a334f988c25fe03cc2348db919b29a63c
Pribalt <vitalisvitalik@gmail.com>
2022-12-12 23:47:36 +0200

    fix_task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -2,15 +2,16 @@ const formEl = document.querySelector('.login-form');
 
 formEl.addEventListener('submit', onForsubmit);
 
-function onForsubmit(evt) {
-   evt.preventDefault();
-   const formElements = evt.currentTarget.elements;
+function onForsubmit(e) {
+   e.preventDefault();
+   const formElements = e.currentTarget.elements;
 
    const email = formElements.email.value;
    const password = formElements.password.value;
 
    if (email === '' || password === '') {
       alert('WARNING! All fields must be filled');
+      return false;
    } else if (email !== '' || password !== '') {
       formEl.reset();
    }
