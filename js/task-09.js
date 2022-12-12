a7bc49a307ef96b807dba1fe77144f7f3e7e84b5
Pribalt <vitalisvitalik@gmail.com>
2022-12-12 21:28:10 +0200

    fix_task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -2,10 +2,16 @@ const buttonChangeColorEl = document.querySelector('.change-color');
 const backgroundColorEl = document.querySelector('.color');
 const body = document.body;
 
-buttonChangeColorEl.addEventListener('click', getRandomHexColor);
+buttonChangeColorEl.addEventListener('click', onTargetButton);
+
+function onTargetButton(e) {
+   backgroundColorEl.textContent = getRandomHexColor();
+   body.style.backgroundColor = getRandomHexColor();
+   console.log(backgroundColorEl.textContent);
+}
 
 function getRandomHexColor() {
-   return (body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215)
+   return `#${Math.floor(Math.random() * 16777215)
       .toString(16)
-      .padStart(6, 0)}`);
+      .padStart(6, 0)}`;
 }
