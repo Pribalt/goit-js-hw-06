9012e3ba759cfc6f7a401100aab1bf15126be8a9
Pribalt <vitalisvitalik@gmail.com>
2022-12-13 12:12:27 +0200

    add_task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
+const numberInputEl = document.querySelector('input[type="number"]');
+const buttonCreateEl = document.querySelector('button[data-create]');
+const buttonDestroyEl = document.querySelector('button[data-destroy]');
+const divBoxesEl = document.querySelector('#boxes');
+
+buttonCreateEl.addEventListener('click', onButtonClickCreateBoxes);
+buttonDestroyEl.addEventListener('click', onButtonClickDestroyBoxes);
+
+function onButtonClickCreateBoxes() {
+   createBoxes(numberInputEl.value);
+}
+
+function createBoxes(amount) {
+   for (let i = 0; i < amount; i += 1) {
+      const divInBoxesEl = document.createElement('div');
+      divInBoxesEl.style.height = 30 + 10 * i + 'px';
+      divInBoxesEl.style.width = divInBoxesEl.style.height;
+      divInBoxesEl.style.backgroundColor = `${getRandomHexColor()}`;
+      divBoxesEl.append(divInBoxesEl);
+   }
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+   return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+}
+
+function onButtonClickDestroyBoxes() {
+   destroyBoxes();
+}
+
+function destroyBoxes() {
+   divBoxesEl.innerHTML = '';
 }
